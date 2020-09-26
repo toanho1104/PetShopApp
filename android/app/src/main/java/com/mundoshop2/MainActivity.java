cacc79bcc8021a21db72458946117a39e3190e2b
Toan Ho <toanho1104@gmail.com>
2020-09-26 12:52:10 +0700

    add-menu-tabbar

--- android/app/src/main/java/com/mundoshop2/MainActivity.java
+++ android/app/src/main/java/com/mundoshop2/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
+    SplashScreen.show(this);
     return "MundoShop2";
   }
 }
---
+++ assets/images/index.js
@@ -0,0 +1,9 @@
+const logo = require('./logo.png')
+const search = require('./search.png')
+const menu = require('./menu.png')
+const home = require('./home.png')
+const anhTest = require('./anhTest.jpg')
+
+export {
+  logo, search, menu, home, anhTest,
+}
